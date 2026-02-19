199b94c71e870b848402db3211397cbe0c3b759e
unknown <ommdivyaranjanpradhan@gmail.com>
2026-02-19 13:50:39 +0530

    db code updated

--- config/db.js
+++ config/db.js
@@ -1,11 +1,24 @@
+// import mongoose from "mongoose";
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect("mongodb://127.0.0.1:27017/library_mgmt");
+//     console.log("✅ MongoDB Connected Successfully");
+//   } catch (error) {
+//     console.error("❌ MongoDB Error:", error.message);
+//     process.exit(1);
+//   }
+// };
+
+// export default connectDB;
 import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/library_mgmt");
-    console.log("✅ MongoDB Connected Successfully");
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected");
   } catch (error) {
-    console.error("❌ MongoDB Error:", error.message);
+    console.error("MongoDB Error:", error.message);
     process.exit(1);
   }
 };
